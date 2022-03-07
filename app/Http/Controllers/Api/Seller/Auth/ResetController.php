26c7d31014e761f9ed6106a415bc532803176bff
nurdin73 <nurdin.reverse73@gmail.com>
2022-03-07 16:19:07 +0700

    update

---
+++ app/Http/Controllers/Api/Seller/Auth/ResetController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Seller\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ResetController extends Controller
+{
+    //
+}
